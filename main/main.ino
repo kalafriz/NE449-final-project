816a4725790fb84c6ff0e56e2fe0161914a69452
Kirsten Alafriz <80020511+kalafriz@users.noreply.github.com>
2023-03-28 15:20:14 -0400

    initial commit

---
+++ main/main.ino
@@ -0,0 +1,17 @@
+// TENS relay control pins
+const int T1_PIN = 2;
+const int T2_PIN = 3;
+const int T3_PIN = 4;
+const int T4_PIN = 5;
+
+///
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
